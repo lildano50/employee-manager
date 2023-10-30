108d2a74c89ce8225255928e7e22dbd0a5bb1946
Daniel Park <danielapark87@gmail.com>
2023-10-30 10:59:22 -0500

    Initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department(name)
+VALUES ("Sales"),
+        ("Billing"),
+        ("Accounts Payable"),
+        ("Administration"),
+        ("Logistics");
+
+INSERT INTO role (title, salary)
+VALUES  ("Lead Sales", 300000),
+        ("Billing Specialist", 20000),
+        ("Accountant", 100000),
+        ("Executive Assistant", 60000),
+        ("Distribution Manager", 80000);
+
+INSERT INTO employee (first_name, last_name)
+VALUES  ("John","Doe"),
+        ("Jane","Smith"),
+        ("Bob","Ross"),
+        ("Dan","Katz"),
+        ("Bill","Self");
