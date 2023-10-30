d7a29a9d978d251a996c070a2b7392701ac1fb50
Daniel Park <danielapark87@gmail.com>
2023-10-30 14:25:03 -0500

    updated db errors and began inquirer intitialization

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ USE employees_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) INT NOT NULL
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
+  title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
   FOREIGN KEY (department_id)
@@ -17,7 +17,7 @@ CREATE TABLE role (
   ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
