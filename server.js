4ae19d52592af9f8f461fdd6fc43897fa8912b7b
Daniel Park <danielapark87@gmail.com>
2023-10-31 19:59:56 -0500

    fixed if-else statements. Need to finish db update/changes

--- server.js
+++ server.js
@@ -28,44 +28,51 @@ function init(promptQuestions){
     inquirer
         .prompt(promptQuestions)
         .then((response) => {
-            while (response !== "Quit") {
                 if (response.initial === "View all departments") {
                     console.log("Viewing Departments:")
                     db.query('SELECT * FROM department;', function (err, results) {
                         console.log(results);
+                        init(promptQuestions);
                       });
-                } else if (response.intial === "View all roles") {
+                } else if (response.initial === "View all roles") {
                     console.log("Viewing Roles:")
                     db.query('SELECT * FROM role;', function (err, results) {
                         console.log(results);
+                        init(promptQuestions);
                       });
-                }  else if (response.intial === "View all employees") {
+                }  else if (response.initial === "View all employees") {
                     console.log("Viewing Employees:")
                     db.query('SELECT * FROM employee;', function (err, results) {
                         console.log(results);
+                        init(promptQuestions);
                       });
-                }  else if (response.intial === "Add a department") {
+                }  else if (response.initial === "Add a department") {
                     // console.log("Viewing Departments:")
                     // db.query('SELECT * FROM department;', function (err, results) {
                     //     console.log(results);
+                    init(promptQuestions);
                     //   });
-                } else if (response.intial === "Add a role") {
+                } else if (response.initial === "Add a role") {
                     // console.log("Viewing Departments:")
                     // db.query('SELECT * FROM department;', function (err, results) {
                     //     console.log(results);
+                    init(promptQuestions);
                     //   });
-                } else if (response.intial === "Add an employee") {
+                } else if (response.initial === "Add an employee") {
                     // console.log("Viewing Departments:")
                     // db.query('SELECT * FROM department;', function (err, results) {
                     //     console.log(results);
+                    init(promptQuestions);
                     //   });
-                } else if (response.intial === "Update an employee role") {
+                } else if (response.initial === "Update an employee role") {
                     // console.log("Viewing Departments:")
                     // db.query('SELECT * FROM department;', function (err, results) {
                     //     console.log(results);
+                    init(promptQuestions);
                     //   });
+                } else {
+                    console.log ("Goodbye!");
                 }
-            };
         });
     }
 
